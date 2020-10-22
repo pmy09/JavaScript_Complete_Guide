2dac031a4fe715058f6ece1c2702151924f9815c
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-22 15:17:07 -0700

    Update app.js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,16 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(x, y){
-    const result = x + y;
-    //alert('The result is '+ result);
-    return result;
-}
+function add(){
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+};
 
-currentResult = add(1, 2);
-
-//let calculationDescription = '('+ defaultResult + ' + 10) * 3 / 2 - 1';
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-//let errorMessage = 'An error occured!'
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
