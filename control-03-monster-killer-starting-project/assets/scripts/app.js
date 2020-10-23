42bb4b254cf1d252415d6fc7359ff0e325ff59ff
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-23 08:10:31 -0700

    Update app.js

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -6,12 +6,21 @@ const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true
 
 adjustHealthBars(chosenMaxLife);
 
 function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+    if (currentPlayerHealth <= 0 && hasBonusLife){
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('Saved by bonus health!');
+    }
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert('You win!');
     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0){
