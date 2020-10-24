2af4ccf2d26b46b2f3a7ccf879b636ffaa3b3b52
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-24 04:06:42 -0700

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -1,23 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-/*const start = function(){
-    console.log('Game is starting!');
-};*/
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK
 
-/*const person = {
-    name: 'Max',
-    greet: function greet(){
-        console.log('Hello there');
+let gameIsRunning = false;
+
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if(
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS 
+    ){
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
     }
-};
+    return selection;
+}
 
-person.greet();*/
-
-//console.log(typeof startGame);
-//console.dir(startGame);
-//startGame()
-
-//startGameBtn.addEventListener('click', start);
 startGameBtn.addEventListener('click', function(){
+    if (gameIsRunning){
+        return;
+    }
+    gameIsRunning = true;
     console.log('Game is starting!');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
 });
