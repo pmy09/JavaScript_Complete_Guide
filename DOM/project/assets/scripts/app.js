070c32fc718b89dab8f1b058d89aa1477665c991
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-24 16:59:36 -0700

    Create app.js

---
+++ DOM/project/assets/scripts/app.js
@@ -0,0 +1,6 @@
+ const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.querySelector('#add-modal');
+//const addMovieModal = document.body.children[1];
+const startAddMovieBtn = document.querySelector('header button');
+// startAddMovieBtn = document.querySelector('header').lastElementChild;
+
