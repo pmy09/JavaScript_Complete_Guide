41142f194871cd94d60997f0085967b7e5f32270
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-24 05:51:48 -0700

    Update app.js

--- functions/app.js
+++ functions/app.js
@@ -84,15 +84,19 @@ startGameBtn.addEventListener('click', () => {
     gameIsRunning = false;
 });
 
+
 // not related to game
 // REST OPERATOR = ...
-const sumUp = (a, b, ...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
+    const validateNum = (number) => {
+        return isNaN(number) ? 0 : number;
+    };
     let sum = 0;
     for (const num of numbers){
-        sum += num;
+        sum += validateNum(num);
     }
-    return sum;
- }
+    resultHandler(sum)
+ };
 
 const subtractUp = function(){
     let sum = 0;
@@ -100,8 +104,12 @@ const subtractUp = function(){
         sum -= num;
     }
     return sum;
-}
+};
 
-console.log(sumUp(1, 5, 10, -3, 6, 10));
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+const showResult = (result) => {
+    alert("The result is: "+ result);
+};
+
+sumUp(showResult, 1, 5, 'hi', -3, 6, 10);
+sumUp(showResult, 1, 5, 10, -3, 6, 10, 25, 88);
 console.log(subtractUp(1, 10, 15, 20));
