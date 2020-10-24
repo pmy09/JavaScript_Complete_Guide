a78a6b33fd6d14d7669c5d6375170192d7b6cd13
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-24 03:30:26 -0700

    Create app.js

---
+++ functions/app.js
@@ -0,0 +1,17 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame(){
+    console.log('Game is starting!');
+}
+
+/*const person = {
+    name: 'Max',
+    greet: function greet(){
+        console.log('Hello there');
+    }
+};
+
+person.greet();*/
+
+//startGame()
+startGameBtn.addEventListener('click', startGame);
