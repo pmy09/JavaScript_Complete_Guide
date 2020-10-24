eeae286bdc98b29af50b385ada06a4760bb88e15
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-23 20:10:26 -0700

    Create app.js

---
+++ behind-scenes-02-block-scope/behind-scenes-02-block-scope/app.js
@@ -0,0 +1,22 @@
+/*let name = 'Max';
+
+if (name === 'Max') {
+  //var hobbies = ['Sports', 'Cooking'];
+  let hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();*/
+
+console.log(userName);
+
+var userName = 'Max'; // works 
+//let userName = 'Max'; // 'let' declaration must come before the call
