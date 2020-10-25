50d9189a48286f0c7886b9375074f121138aa8f9
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-24 17:25:45 -0700

    Update app.js

--- DOM/project/assets/scripts/app.js
+++ DOM/project/assets/scripts/app.js
@@ -3,9 +3,27 @@
 //const addMovieModal = document.body.children[1];
 const startAddMovieBtn = document.querySelector('header button');
 // startAddMovieBtn = document.querySelector('header').lastElementChild;
+const backdrop = document.getElementById('backdrop');
+//const backdrop = document.body.firstElementChild;
+const cancelAddMovieBtn = addMovieModal.querySelector('.btn--passive')
+
+function toggleBackdrop(){
+    backdrop.classList.toggle('visible');
+};
 
 function toggleMovieModal(){
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
-startAddMovieBtn.addEventListener('click', toggleMovieModal());
+function cancelAddMovie(){
+    toggleMovieModal();
+};
+
+function backdropClickHandler(){
+    toggleMovieModal();
+};
+
+startAddMovieBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieBtn.addEventListener('click', cancelAddMovie);
