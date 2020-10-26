d160c6128ad04764f8b88f99cc7b05aae69b6c0e
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 05:05:04 -0700

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -110,3 +110,9 @@ const sortedPrices = prices.sort((a, b) => {
 });
 //console.log(sortedPrices.reverse());
 console.log(sortedPrices);
+
+const filteredArray = prices.filter((price, idx, prices) =>{
+    return price > 6;
+});
+
+console.log(filteredArray);
