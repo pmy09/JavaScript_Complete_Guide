24586d0be041b811d51414cab5260f48712bd0cb
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 05:01:15 -0700

    sorting prices

--- arrays/app.js
+++ arrays/app.js
@@ -97,5 +97,16 @@ const taxAdjustedPrices = prices.map((price, idx, prices) => {
     return priceObj;
 });
 
-console.log(prices, taxAdjustedPrices);
+//console.log(prices, taxAdjustedPrices);
 
+const sortedPrices = prices.sort((a, b) => {
+    if (a > b){
+        return -1;
+    } else if (a === b){
+        return 0;
+    }else {
+        return 1;
+    }
+});
+//console.log(sortedPrices.reverse());
+console.log(sortedPrices);
