64ef57a31b7fbf59b0a16974ec6770a676f716ff
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 13:46:46 -0700

    Update objects.js

--- objects/assets/scripts/objects.js
+++ objects/assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style['backgroundColor'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 const person = {
   'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  [userChosenKeyName]: '...',
   greet: function() {
     alert('Hi there!');
   },
@@ -17,9 +20,11 @@ const person = {
 delete person.age;
 person.isAdmin = true;
 
-
 //person.greet();
 
+const keyName = 'first name';
+
+console.log(person[keyName]);
 console.log(person['first name']);
 console.log(person[1.5]);
 console.log(person);
