83954bbf9bb824bcd5cbed5e3bd218b3b26abfd1
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 04:54:28 -0700

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -74,17 +74,28 @@
 
 // console.log(maxIndex);
 
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
+// const taxAdjustedPrices =[];
+
+// // for (const price of prices){
+// //     taxAdjustedPrices.push(price * (1 + tax));
+// // };
+
+// prices.forEach((price, idx, prices) => {
+//     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+//     taxAdjustedPrices.push(priceObj);
+// });
+
+// console.log(taxAdjustedPrices);
+
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices =[];
 
-// for (const price of prices){
-//     taxAdjustedPrices.push(price * (1 + tax));
-// };
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
-    taxAdjustedPrices.push(priceObj);
+    return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
+
