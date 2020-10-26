a30a120b05d446c9afec0c47963a576fa51f091f
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 04:50:38 -0700

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -44,32 +44,47 @@
 // const removedElements = hobbies.splice(-2, 1);
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
-//const storedResults = testResults.slice;
-//const storedResults = testResults.slice(2);
-const storedResults = testResults.concat([3.99, 2]);
+// const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// //const storedResults = testResults.slice;
+// //const storedResults = testResults.slice(2);
+// const storedResults = testResults.concat([3.99, 2]);
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(1.5));
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5));
 
-console.log(testResults.includes(10.99));
-console.log(testResults.indexOf(10.99) != -1);
+// console.log(testResults.includes(10.99));
+// console.log(testResults.indexOf(10.99) != -1);
 
-const personData = [{name: 'Max'}, {name: 'Manuel'}];
-console.log(personData.indexOf({name: 'Manuel'})); // indexOf doesn't work on objects
+// const personData = [{name: 'Max'}, {name: 'Manuel'}];
+// console.log(personData.indexOf({name: 'Manuel'})); // indexOf doesn't work on objects
 
-const manuel = personData.find((person, idx, persons) => {
-    return person.name === 'Manuel';
+// const manuel = personData.find((person, idx, persons) => {
+//     return person.name === 'Manuel';
+// });
+
+// manuel.name = 'Anna'
+
+// console.log(manuel, personData);
+
+// const maxIndex = personData.findIndex((person, idx, persons) => {
+//     return person.name === 'Max';
+// });
+
+// console.log(maxIndex);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices =[];
+
+// for (const price of prices){
+//     taxAdjustedPrices.push(price * (1 + tax));
+// };
+
+prices.forEach((price, idx, prices) => {
+    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+    taxAdjustedPrices.push(priceObj);
 });
 
-manuel.name = 'Anna'
-
-console.log(manuel, personData);
-
-const maxIndex = personData.findIndex((person, idx, persons) => {
-    return person.name === 'Max';
-});
-
-console.log(maxIndex);
+console.log(taxAdjustedPrices);
