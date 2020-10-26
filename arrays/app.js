b60c945c6d25acae9205c7e21b04e7428f96bbb1
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 03:58:34 -0700

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -38,3 +38,8 @@ hobbies[1] = 'Coding';
 hobbies[5] = 'Reading';
 console.log(hobbies, hobbies[4]);
 
+hobbies.splice(1, 0, 'Good Food');
+console.log(hobbies);
+
+const removedElements = hobbies.splice(-2, 1);
+console.log(hobbies);
