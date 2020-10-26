2b360a7854fb367da92d5c1aaec9dd0b49367a40
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 13:25:21 -0700

    Create objects.js

---
+++ objects/assets/scripts/objects.js
@@ -0,0 +1,15 @@
+const person = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  greet: function() {
+    alert('Hi there!');
+  }
+};
+
+//person.age = 31;
+delete person.age;
+person.isAdmin = true;
+console.log(person);
+
+person.greet();
