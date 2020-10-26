3d0534fd50ec0e5ac9d7928d5bfb4d0928eab0d1
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 06:18:45 -0700

    Create setsmaps.js

---
+++ sets-maps/setsmaps.js
@@ -0,0 +1,12 @@
+const ids = new Set(['Hi', 'from', 'set']);
+ids.add(2);
+console.log(ids);
+if (ids.has('Hi')){
+    ids.delete('Hi');
+};
+
+for (const entry of ids.entries()) {
+    console.log(entry[0]);
+}
+
+
