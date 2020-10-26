15d87da3516a05d6aea6309f96789e40a998acfe
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 04:34:47 -0700

    Update app.js

--- arrays/app.js
+++ arrays/app.js
@@ -56,3 +56,17 @@ console.log(testResults.indexOf(1.5));
 
 const personData = [{name: 'Max'}, {name: 'Manuel'}];
 console.log(personData.indexOf({name: 'Manuel'})); // indexOf doesn't work on objects
+
+const manuel = personData.find((person, idx, persons) => {
+    return person.name === 'Manuel';
+});
+
+manuel.name = 'Anna'
+
+console.log(manuel, personData);
+
+const maxIndex = personData.findIndex((person, idx, persons) => {
+    return person.name === 'Max';
+});
+
+console.log(maxIndex);
