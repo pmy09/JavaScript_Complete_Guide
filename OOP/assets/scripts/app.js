e915c5d743a5f7b58268576218e04a1de9b5bf25
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 19:38:32 -0700

    Update app.js

--- OOP/assets/scripts/app.js
+++ OOP/assets/scripts/app.js
@@ -9,13 +9,34 @@ class Product {
         this.imageUrl = image;
         this.description = desc;
         this.price = price;
+    };
+}
+
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    };
+
+    render(){
+        const prodEl = document.createElement('li');
+            prodEl.className = 'product- item';
+            prodEl.innerHTML = `
+                <div>
+                    <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                    <div class="product-item_content">
+                        <h2>${this.product.title}</h2>
+                        <h3>\$${this.product.price}</h3>
+                        <p>${this.product.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
+        return prodEl; 
     }
 }
 
-console.log(new Product);
-
-const productList = {
-    products: [
+class ProductList {
+    products = [
         new Product(
             'A Pillow',
             'https://thumb.maxpixel.net/50/Sleep-Bedtime-Pillow-Bedroom-Dream-Comfortable-1738023.jpg',
@@ -27,30 +48,23 @@ const productList = {
             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flower_carpet_Pookkalam_Onappookkalam_at_Nithyananda_Ashram_Hosdurg_2019.jpg/120px-Flower_carpet_Pookkalam_Onappookkalam_at_Nithyananda_Ashram_Hosdurg_2019.jpg',
             'A carpet you might like',
             89.99
-        ),
-    ],
+        ),  
+    ];
+
+    constructor(){};
+
     render(){
         const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products){
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product- item';
-            prodEl.innerHTML = `
-                <div>
-                    <img src="${prod.imageUrl}" alt="${prod.title}">
-                    <div class="product-item_content">
-                        <h2>${prod.title}</h2>
-                        <h3>\$${prod.price}</h3>
-                        <p>${prod.description}</p>
-                        <button>Add to Cart</button>
-                    </div>
-                </div>
-            `;
+            const productItem = new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
-    }
-};
+    };
+}
 
+const productList = new ProductList();
 productList.render();
