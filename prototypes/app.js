ec665b2ee30a3c0f46c72a18408e54bc8811ce6d
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-27 08:11:13 -0700

    Update app.js

--- prototypes/app.js
+++ prototypes/app.js
@@ -12,13 +12,13 @@ class Person {
         this.age = 30;
     }
 
-    greet = () => {
-        console.log('Hi, I am '+ this.name +' and I am '+ this.age + ' years old.');
-    };
-
-    // greet(){
+    // greet = () => {
     //     console.log('Hi, I am '+ this.name +' and I am '+ this.age + ' years old.');
-    // }
+    // };  // a1
+
+    greet(){
+        console.log('Hi, I am '+ this.name +' and I am '+ this.age + ' years old.');
+    }
 }
 
 // function Person(){
@@ -49,10 +49,54 @@ class Person {
 // const p = new person.__proto__.constructor();
 // console.dir(Object.prototype);
 
-const p = new Person();
-const p2 = new Person();
-p.greet();
-console.log(p.__proto__ === p2.__proto__);
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p.__proto__ === p2.__proto__);
 
-const button = document.getElementById('btn');
-button.addEventListener('click', p.greet);
+// const button = document.getElementById('btn');
+// //button.addEventListener('click', p.greet);   // with a1
+// button.addEventListener('click', p.greet.bind(p));
+
+const course = {
+    title: 'Java',
+    rating: 5
+};
+
+//console.log(Object.getPrototypeOf(course));
+//console.log(course.__proto__);
+
+Object.setPrototypeOf(course, {
+    ...Object.getPrototypeOf(course),
+    printRating: function(){
+        console.log(`${this.rating}/5`);
+    }
+});
+
+const student = Object.create({
+    printProgress: function(){
+        console.log(this.progress);
+    }
+}, {
+    name: {
+    configurable: true,
+    enumerable: true,
+    value: 'Max',
+    writable: true
+    }
+});
+
+// student.name = "Max";
+
+Object.defineProperty(student, 'progress', {
+    configurable: true,
+    enumerable: true,
+    value: 0.8,
+    writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
