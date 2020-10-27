1f423a7a844aa00de5f137f8e65470549d3bfcfb
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-26 21:23:10 -0700

    Update app.js

--- OOP/assets/scripts/app.js
+++ OOP/assets/scripts/app.js
@@ -12,7 +12,33 @@ class Product {
     };
 }
 
-class ShoppingCart {
+class ElementAttribute {
+    constructor(attrName, attrValue){
+        this.name = attrName;
+        this.value = attrValue;
+    }
+}
+
+class Component {
+    constructor(renderHook){
+        this.hookId = renderHook;
+    }
+    createRootElement(tag, cssClasses, attributes){
+        const rootElement = document.createElement(tag);
+        if (cssClasses){
+            rootElement.className = cssClasses;
+        }
+        if (attributes && attributes.length > 0){
+            for (const attr of attributes){
+                rootElement.setAttribute(attr.name, attr.value);
+            }
+        }
+        document.getElementById(this.hookId).append(rootElement);
+        return rootElement;
+    }
+}
+
+class ShoppingCart extends Component {
     items = [];
 
     set cartItems(value){
@@ -28,6 +54,10 @@ class ShoppingCart {
         return sum;
     }
 
+constructor(renderHookId){
+    super(renderHookId);
+}
+
     addProduct(product){
         const updatedItems = [...this.items];
         updatedItems.push(product);
@@ -35,12 +65,12 @@ class ShoppingCart {
     }
 
     render(){
-        const cartEl = document.createElement('section');
+        const cartEl = this.createRootElement('section', 'cart');
+        //const cartEl = document.createElement('section');
         cartEl.innerHTML = `
             <h2>Total: \$${0}</h2>
             <button>Order Now!</button>
         `;
-        cartEl.className = 'cart';
         this.totalOutput = cartEl.querySelector('h2');
         return cartEl;
     }
@@ -108,8 +138,8 @@ class ProductList {
 class Shop {
     render(){
         const renderHook = document.getElementById('app');
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
+        this.cart = new ShoppingCart(app);
+        this.cart.render();
         const productList = new ProductList();
         const prodListEl = productList.render();
 
