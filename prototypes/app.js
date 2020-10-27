2905b73eb6ec13a8706756e38c36dfcc9af1b98b
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-27 06:43:54 -0700

    Update app.js

--- prototypes/app.js
+++ prototypes/app.js
@@ -1,7 +1,14 @@
-// class Person {
+// class AgedPerson {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// }
+
+// class Person extends AgedPerson {
 //     name = 'Max';
 
 //     constructor(){
+//         super();
 //         this.age = 30;
 //     }
 //     greet(){
@@ -14,8 +21,18 @@ function Person(){
     this.name = 'Max';
     this.greet = function(){
         console.log('Hi, I am '+ this.name +' and I am '+ this.age + ' years old.');
-    }
+    };
 }
+
+Person.prototype = {
+    printAge() {
+      console.log(this.age);
+    }
+};
+
+console.dir(Person);
 
 const person = new Person();
 person.greet();
+person.printAge();
+console.log(person.__proto__);
