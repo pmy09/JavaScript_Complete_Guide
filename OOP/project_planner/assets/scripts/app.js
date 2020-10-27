d567952eb70da6126b781ec085234fc4716d2fd8
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-27 16:17:08 -0700

    Update app.js

--- OOP/project_planner/assets/scripts/app.js
+++ OOP/project_planner/assets/scripts/app.js
@@ -50,7 +50,7 @@ class ProjectItem {
         const tooltipText = projectElement.dataset.extraInfo;
         const tooltip = new ToolTip(() => {
             this.hasActiveToolTip = false;
-        }, tooltipText);
+        }, tooltipText, this.id);
         tooltip.attach();
         this.hasActiveToolTip = true;
     }
@@ -113,8 +113,8 @@ class Component {
 }
 
 class ToolTip extends Component {
-    constructor(closeNotifierFunction, text) {
-        super();
+    constructor(closeNotifierFunction, text, hostElementId) {
+        super(hostElementId);
         this.closeNotifier = closeNotifierFunction;
         this.text = text;
         this.create();
@@ -129,6 +129,20 @@ class ToolTip extends Component {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
         tooltipElement.textContent = this.text;
+        //console.log(this.hostElement.getBoundingClientRect());
+
+        const hostElPosLeft = this.hostElement.offsetLeft;
+        const hostElPosTop = this.hostElement.offsetTop;
+        const hostElHeight = this.hostElement.clientHeight;
+        const parentElementScrolling = this.hostElement.parentElement.scrollTop;
+
+        const x = hostElPosLeft + 20;
+        const y = hostElPosTop + hostElHeight - parentElementScrolling - 10;
+
+        tooltipElement.style.position = 'absolute';
+        tooltipElement.style.left = x + 'px';
+        tooltipElement.style.top = y + 'px';
+
         tooltipElement.addEventListener('click', this.closeToolTip.bind(this));
         this.element = tooltipElement;
     }
