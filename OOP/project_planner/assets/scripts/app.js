6e352163db2ea91dd42bfeb973f0b3a921453485
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-27 12:01:13 -0700

    Update app.js

--- OOP/project_planner/assets/scripts/app.js
+++ OOP/project_planner/assets/scripts/app.js
@@ -1,13 +1,40 @@
 class ProjectList {
+    projects =[];
+
     constructor(type) {
         const projItems = document.querySelectorAll(`#${type}-projects li`);
-        console.log(projItems);
+        for (const projItem of projItems){
+            this.projects.push(new ProjectItem(projItem.id));
+        }
+        console.log(this.projects);
+    }
+
+    addProject() {}
+
+    switchProject(projectId) {
+        // const projectIndex = this.projects.findIndex(p => p.id === projectId);
+        // this.projects.splice(projectIndex, 1);
+        this.projects = this.projects.filter(p => p.id !== projectId);
     }
 }
 
-//class ProjectItem {}
+class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
+    }
 
-//class ToolTip {}
+    connectMoreInfoButton() {}
+
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector('button: last-of-type');
+        switchBtn.addEventListener('click', );
+    }
+}
+
+class ToolTip {}
 
 class App {
     static init(){
