121d0ba978e886a2dfcfb29b592b5b4203818854
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-28 05:29:07 -0700

    Update events.js

--- JavaScript_Complete_Guide_2020/OOP/project_planner/assets/scripts/events.js
+++ JavaScript_Complete_Guide_2020/OOP/project_planner/assets/scripts/events.js
@@ -1,27 +1,40 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 //button.onclick = function(){};
 
-const buttonClickHandler = () => {
-    alert('Button was clicked!');
+// const buttonClickHandler = () => {
+//     alert('Button was clicked!');
+// };
+
+// const anotherButtonClickHandler = () => {
+//     console.log('This was clicked!')
+// }
+
+const buttonClickHandler = event => {
+    event.target.disabled = true;
+    console.log(event);
 };
 
 const anotherButtonClickHandler = () => {
     console.log('This was clicked!')
 }
 
-// button.onclick = buttonClickHandler;
-// button.onclick = anotherButtonClickHandler;
+//button.onclick = buttonClickHandler;
+//button.onclick = anotherButtonClickHandler;
 
-// button.addEventListener('click', buttonClickHandler);
+//button.addEventListener('click', buttonClickHandler);
 
 // setTimeout(() => {
 //     button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
 // // using .bind(this) in each here will not work as it creates 2 diff funcs. Use below
 
-const boundFn = buttonClickHandler.bind(this);
-button.addEventListener('click', boundFn);
-setTimeout(() => {
-    button.removeEventListener('click', boundFn);
-}, 2000);
+// const boundFn = buttonClickHandler.bind(this);
+// button.addEventListener('click', boundFn);
+// setTimeout(() => {
+//     button.removeEventListener('click', boundFn);
+// }, 2000);
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', buttonClickHandler)
+});
