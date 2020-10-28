d0146bb6bd6a4d3f1e9d42aa16ffd4f3b64edbf2
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-28 05:00:46 -0700

    Create events.js

---
+++ JavaScript_Complete_Guide_2020/OOP/project_planner/assets/scripts/events.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button');
+
+//button.onclick = function(){};
+
+// const buttonClickHandler = () => {
+//     alert('Button was clicked!');
+// };
+
+// const anotherButtonClickHandler = () => {
+//     console.log('This was clicked!')
+// }
+
+// button.onclick = buttonClickHandler;
+// button.onclick = anotherButtonClickHandler;
+
+button.addEventListener();
+
+button.removeEventListener();
