92db81c6cfde47e0c4c89a6a3f344491c0e27610
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-28 04:33:30 -0700

    Create app.js

---
+++ JavaScript_Complete_Guide_2020/block-scope/app.js
@@ -0,0 +1,26 @@
+/*let name = 'Max';
+
+if (name === 'Max') {
+  //var hobbies = ['Sports', 'Cooking'];
+  let hobbies = ['Sports', 'Cooking'];
+  console.log(hobbies);
+}
+
+function greet() {
+  let age = 30;
+  let name = 'Manuel';
+  console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();*/
+
+'use strict';
+
+const userName = 'Max'
+//var undefined = 5 // won't work with strict mode
+console.log(userName);
+
+//var userName = 'Max'; // works 
+//let userName = 'Max'; // 'let' and 'const' declaration must come before the call
