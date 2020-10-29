b68eeb0ee16b73e83d33b5b62fca07e15e63c87f
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-29 15:10:33 -0700

    Update app.js

--- JavaScript_Complete_Guide_2020/http requests/assets/scripts/app.js
+++ JavaScript_Complete_Guide_2020/http requests/assets/scripts/app.js
@@ -13,10 +13,20 @@ function sendHttpRequest(method, url, data) {
         xhr.responseType = 'json';
 
         xhr.onload = function() {
-            resolve(xhr.response);
-            //console.log(xhr.response);
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+                //console.log(xhr.response);
+            } else {
+                reject(new Error('Something went wrong!'));
+            }               
             // const listOfPosts = JSON.parse(xhr.response);
             // console.log(listOfPosts);
+        };
+
+        xhr.onerror = function() {
+            console.log(xhr.response);
+            console.log(xhr.status);
+            reject(new Error('Failed to send request!'));
         };
 
         xhr.send(JSON.stringify(data));
@@ -26,17 +36,21 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
-    const responseData = await sendHttpRequest(
-        'GET',
-        'https://jsonplaceholder.typicode.com/posts'
-    );
-    const listOfPosts = responseData;
-    for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        postEl.querySelector('li').id = post.id;
-        listElement.append(postEl);
+    try {
+        const responseData = await sendHttpRequest(
+            'GET',
+            'https://jsonplaceholder.typicode.com/posts'
+        );
+        const listOfPosts = responseData;
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            postEl.querySelector('li').id = post.id;
+            listElement.append(postEl);
+        }
+    } catch (error) {
+        alert(error.message);
     }
 };
 
