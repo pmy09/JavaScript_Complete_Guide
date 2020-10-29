54d6283ebc1c4d0a2cc344f9ec9615e6a00b58d3
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-29 14:57:34 -0700

    Update app.js

--- JavaScript_Complete_Guide_2020/http requests/assets/scripts/app.js
+++ JavaScript_Complete_Guide_2020/http requests/assets/scripts/app.js
@@ -2,6 +2,7 @@ const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
     const promise = new Promise((resolve, reject) => {
@@ -34,6 +35,7 @@ async function fetchPosts() {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
         postEl.querySelector('p').textContent = post.body;
+        postEl.querySelector('li').id = post.id;
         listElement.append(postEl);
     }
 };
@@ -51,14 +53,22 @@ async function createPost(title, content) {
 
 fetchButton.addEventListener('click', fetchPosts);
 form.addEventListener('submit', event => {
-  event.preventDefault();
-  const enteredTitle = event.currentTarget.querySelector('#title').value;
-  const enteredContent = event.currentTarget.querySelector('#content').value;
+    event.preventDefault();
+    const enteredTitle = event.currentTarget.querySelector('#title').value;
+    const enteredContent = event.currentTarget.querySelector('#content').value;
 
   createPost(enteredTitle, enteredContent);
 });
 
-
+postList.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        //console.log('Clicked on Button');
+        const postId = event.target.closest('li').id;
+        //console.log(postId);
+        sendHttpRequest('DELETE', `https://jsonplaceholder.typicode.com/posts/${postId}`);
+    }
+  });
+  
     
 
 
