971dce244f21e90e8f32021c73dfc0c28827e0be
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-29 10:35:33 -0700

    Update app.js

--- JavaScript_Complete_Guide_2020/promises/app.js
+++ JavaScript_Complete_Guide_2020/promises/app.js
@@ -25,24 +25,26 @@ const setTimer = duration => {
   return promise;
 };
 
-function trackUserHandler() {
-    let positionData;
-    getPosition()
-      .then(posData => {
-        positionData = posData;
-        return setTimer(2000);
-      })
-      .catch((err) => {
-        console.log(err);
-        return 'on we go...';
-      })
-      .then(data => {
-        console.log(data, positionData);
-      });
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  });
-  console.log('Getting position...');
+async function trackUserHandler() {
+    //let positionData;
+    const posData = await getPosition();
+    const timerData = await setTimer(2000);
+    console.log(timerData, posData);
+    //   .then(posData => {
+    //     positionData = posData;
+    //     return setTimer(2000);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     return 'on we go...';
+    //   })
+    //   .then(data => {
+    //     console.log(data, positionData);
+    //   });
+//   setTimer(1000).then(() => {
+//     console.log('Timer done!');
+//   });
+//   console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler);
