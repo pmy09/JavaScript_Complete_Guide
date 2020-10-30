8331836c0b75fd0b9cc3cd828fb3c2b16961de34
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-30 13:29:03 -0700

    Update ProjectList.js

--- JavaScript_Complete_Guide_2020/modules/assets/scripts/App/ProjectList.js
+++ JavaScript_Complete_Guide_2020/modules/assets/scripts/App/ProjectList.js
@@ -1,4 +1,6 @@
-class ProjectList {
+import {ProjectItem} from './ProjectItem.js';
+import {DOMHelper} from '../Utility/DOMHelper.js'
+export class ProjectList {
     projects =[];
 
     constructor(type) {
