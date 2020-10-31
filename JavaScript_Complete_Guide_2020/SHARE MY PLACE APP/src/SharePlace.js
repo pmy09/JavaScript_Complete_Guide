187d8cc14f300df776064e7c68e018e175a7a64f
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-31 14:52:38 -0700

    Update SharePlace.js

---
+++ JavaScript_Complete_Guide_2020/SHARE MY PLACE APP/src/SharePlace.js
@@ -0,0 +1,37 @@
+class PlaceFinder {
+    constructor() {
+        const addressForm = document.querySelector('form');
+        const locateUserBtn = document.getElementById('locate-btn');
+  
+        locateUserBtn.addEventListener('click', this.locateUserHandler);
+        addressForm.addEventListener('submit', this.findAddressHandler);
+    }
+  
+    locateUserHandler() {
+        if (!navigator.geolocation) {
+            alert(
+            'Location feature is not available in your browser - please use a more modern browser or manually enter an address.'
+            );
+            return;
+        }
+        navigator.geolocation.getCurrentPosition(
+            successResult => {
+                console.log(successResult);
+                const coordinates = {
+                    lat: successResult.coords.latitude + Math.random() * 50, // Math.random not required
+                    lng: successResult.coords.longitude + Math.random() * 50, // used to hide Max's location
+                };
+                console.log(coordinates);
+            },
+            error => {
+                alert(
+                    'Could not locate you unfortunately. Please enter an address manually!'
+                );
+            }
+        );
+    }
+  
+    findAddressHandler() {}
+}
+  
+const placeFinder = new PlaceFinder();
