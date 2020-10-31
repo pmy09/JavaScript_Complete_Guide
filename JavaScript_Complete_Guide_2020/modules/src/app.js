657b793c2f968d253f4928842766894e9c5270b3
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-31 11:37:28 -0700

    Create app.js

---
+++ JavaScript_Complete_Guide_2020/modules/src/app.js
@@ -0,0 +1,32 @@
+import * as _ from 'lodash/array';
+import {ProjectList} from './App/ProjectList';
+
+console.log(_.difference([0, 1], [1, 2]));
+
+class App {
+    static init(){
+        const activeProjectList = new ProjectList('active');
+        const finishedProjectList = new ProjectList('finished');
+        activeProjectList.setSwitchHandlerFunction(finishedProjectList.addProject.bind(finishedProjectList));
+        finishedProjectList.setSwitchHandlerFunction(activeProjectList.addProject.bind(activeProjectList));
+
+        // const someScript = document.createElement('script');
+        // someScript.textContent = 'alert("Hi there");';
+        // document.head.append(someScript);
+
+        // const timerId = setTimeout(this.startAnalytics, 3000);
+        // //document.getElementById('start-analytics-btn').addEventListener('click', this.startAnalytics);
+        // document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+        //     clearTimeout(timerId);
+        // });
+    }
+
+    static startAnalytics() {
+        const analyticsScript = document.createElement('script');
+        analyticsScript.src = 'assets/scripts/Utility/analytics.js';
+        analyticsScript.defer = true;
+        document.head.append(analyticsScript);
+    }
+}
+
+App.init();
