d0d6fe6abe905e9c9e41c0588cd656eac927e90b
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-31 11:37:19 -0700

    Delete analytics.js

--- JavaScript_Complete_Guide_2020/modules/assets/scripts/Utility/analytics.js
+++
@@ -1,9 +0,0 @@
-
-const intervalId = setInterval(() => {
-    console.log('Sending analytics data ...')
-}, 2000);
-
-document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-    clearInterval(intervalId);
-    //location.href = 'https://academind.com'; // transports you to the new page.
-});
