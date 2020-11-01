8e1e29fda2f36fa561ef6764890bf4da10d70eb4
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-11-01 02:55:06 -0800

    Update app.js

--- JavaScript_Complete_Guide_2020/meta-programming/app.js
+++ JavaScript_Complete_Guide_2020/meta-programming/app.js
@@ -19,3 +19,27 @@ user.id = 'p2'; // this should not be possible!
 console.log(user[Symbol('uid')]);
 console.log(Symbol('uid') === Symbol('uid'));
 console.log(user.toString());
+
+const company = {
+    curEmployee: 0,
+    employees: ['Max', 'Manu', 'Anna'],
+    next() {
+      if (this.curEmployee >= this.employees.length) {
+        return { value: this.curEmployee, done: true };
+      }
+      const returnValue = {
+        value: this.employees[this.curEmployee],
+        done: false
+      };
+      this.curEmployee++;
+      return returnValue;
+    }
+  };
+  
+  let employee = company.next();
+  
+  while(!employee.done) {
+    console.log(employee.value);
+    employee = company.next();
+  }
+  
