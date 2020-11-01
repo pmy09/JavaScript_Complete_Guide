3efacd4906a631ae54120c77b6e3f41f01a98979
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-11-01 03:58:12 -0800

    Update app.js

--- JavaScript_Complete_Guide_2020/node_js/app.js
+++ JavaScript_Complete_Guide_2020/node_js/app.js
@@ -1,3 +1,21 @@
-const userName = 'Max';
+// const userName = 'Max';
 
-console.log(`Hi ${userName}!`);
+// console.log(`Hi ${userName}!`);
+
+const fs = require('fs');
+
+fs.readFile('user-data.txt', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data.toString());
+});
+
+fs.writeFile('user-data.txt', 'username=Max', err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Wrote to file!');
+    }
+});
