b044a058d0f8595cd8694fbbbb74244dda27214e
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-10-31 17:55:48 -0700

    Update SharePlace.js

--- JavaScript_Complete_Guide_2020/SHARE MY PLACE APP/src/SharePlace.js
+++ JavaScript_Complete_Guide_2020/SHARE MY PLACE APP/src/SharePlace.js
@@ -1,21 +1,45 @@
 import { Modal } from './UI/Modal';
 import { Map } from './UI/Map';
+import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
+
 
 class PlaceFinder {
     constructor() {
         const addressForm = document.querySelector('form');
         const locateUserBtn = document.getElementById('locate-btn');
+        this.shareBtn = document.getElementById('share-btn');
   
         locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+        this.shareBtn.addEventListener('click', this.sharePlaceHandler);
         addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
     }
 
-    selectPlace(coordinates) {
+    sharePlaceHandler() {
+        const sharedLinkInputElement = document.getElementById('share-link');
+        if (!navigator.clipboard) {
+            sharedLinkInputElement.select();
+            return;
+        }
+
+        navigator.clipboard.writeText(sharedLinkInputElement.value)
+            .then(() => {
+                alert('Copied into clipboard!');
+            })
+            .catch(err => {
+                console.log(err);
+                sharedLinkInputElement.select();
+            });
+    }
+
+    selectPlace(coordinates, address) {
         if (this.map) {
             this.map.render(coordinates);
         } else {
             this.map = new Map(coordinates);
         }
+        this.shareBtn.disabled = false;
+        const sharedLinkInputElement = document.getElementById('share-link');
+        sharedLinkInputElement.value = `${location.origin}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${coordinates.lng}`;
     }
   
     locateUserHandler() {
@@ -28,14 +52,16 @@ class PlaceFinder {
         const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
         modal.show();
         navigator.geolocation.getCurrentPosition(
-            successResult => {
+            async successResult => {
                 console.log(successResult);
-                modal.hide();
+                //modal.hide();
                 const coordinates = {
                     lat: successResult.coords.latitude, //+ Math.random() * 50, // Math.random not required
                     lng: successResult.coords.longitude //+ Math.random() * 50, // used to hide Max's location
                 };
-                this.selectPlace(coordinates);
+                const address = await getAddressFromCoords(coordinates);
+                modal.hide();
+                this.selectPlace(coordinates, address);
                 console.log(coordinates);
             },
             error => {
@@ -47,7 +73,7 @@ class PlaceFinder {
         );
     }
   
-    findAddressHandler(event) {
+    async findAddressHandler(event) {
         event.preventDefault();
         const address = event.target.querySelector('input').value;
         if (!address || address.trim().length === 0) {
@@ -59,7 +85,13 @@ class PlaceFinder {
             'Loading location - please wait!'
         );
         modal.show();
-        
+        try {
+            const coordinates = await getCoordsFromAddress(address);
+            this.selectPlace(coordinates);
+        } catch (err) {
+            alert(err.message);
+        }
+        modal.hide();
     }
 }
   
