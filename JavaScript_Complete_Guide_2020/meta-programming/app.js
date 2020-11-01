b694ad83cc971f4bf98fc09cf1efc66f7d6d87b0
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-11-01 03:22:51 -0800

    Update app.js

--- JavaScript_Complete_Guide_2020/meta-programming/app.js
+++ JavaScript_Complete_Guide_2020/meta-programming/app.js
@@ -74,3 +74,23 @@ for (const employee of company) {
 
 const persons = ['Max', 'Manu'];
 console.log(persons);
+
+// ---
+
+const course = {
+    title: 'JavaScript - The Complete Guide'
+};
+  
+Reflect.setPrototypeOf(course, {
+    toString() {
+        return this.title;
+    }
+});
+  
+Reflect.deleteProperty(course, 'title');
+  
+// Object.deleteProperty(course, 'title');
+  
+// delete course.title;
+  
+console.log(course);
