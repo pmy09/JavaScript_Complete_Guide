509394797abf2ce10e778cf52f383562cbede58e
pmy09 <69659683+pmy09@users.noreply.github.com>
2020-11-01 07:34:37 -0800

    Update MyPlace.js

--- JavaScript_Complete_Guide_2020/SHARE MY PLACE APP/src/MyPlace.js
+++ JavaScript_Complete_Guide_2020/SHARE MY PLACE APP/src/MyPlace.js
@@ -10,9 +10,22 @@ class LoadedPlace {
 
 const url = new URL(location.href);
 const queryParams = url.searchParams;
-const coords = {
-  lat: parseFloat(queryParams.get('lat')),  // both do
-  lng: +queryParams.get('lng')              // the same thing
-};
-const address = queryParams.get('address');
-new LoadedPlace(coords, address);
+// const coords = {
+//   lat: parseFloat(queryParams.get('lat')),  // both do
+//   lng: +queryParams.get('lng')              // the same thing
+// };
+// const address = queryParams.get('address');
+const locId = queryParams.get('location');
+fetch('http://localhost:3000/location/' + locId)
+  .then(response => {
+    if (response.status === 404) {
+      throw new Error('Could not find location!')
+    }
+    return response.json();
+  })
+  .then(data => {
+    new LoadedPlace(data.coordinates, data.address);
+  })
+  .catch(err => {
+    alert(err.message);
+  }); 
